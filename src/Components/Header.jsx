00db018b6d0925e21899857373fdab978960cd25
Lakshya06 <lakshyagupta068@gmail.com>
2024-06-30 21:42:41 +0530

    Completed Navbar and homepage

---
+++ src/Components/Header.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+// import { Link, Navigate } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Image from 'react-bootstrap/Image';
+
+function Header(){
+
+    return (
+        <header>
+            <Navbar expand="lg" className="acme-regular" data-bs-theme="dark" sticky="top">
+            <Container>
+                {/* <Navbar.Brand href="#home"></Navbar.Brand> */}
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto m-auto navbar-dark mt-2 mt-md-0">
+                    <Nav.Link href="#home" className="mx-4 my-auto active">HOME</Nav.Link>
+                    <Nav.Link href="#" className="mx-4 my-auto">STORE</Nav.Link>
+                    <Nav.Link href="#" className="mx-4 my-auto">DISCORD</Nav.Link>
+
+                    {/* <Navbar.Brand href="#home"> */}
+                    <Image src={require("../Assets/logo3.png")} alt="" className="logo my-2 mx-5 d-none d-lg-block" fluid/>
+                    {/* </Navbar.Brand> */}
+                    
+
+                    <Nav.Link href="#" className="mx-4 my-auto">FORUM</Nav.Link>
+                    <Nav.Link href="#" className="mx-4 my-auto">SUPPORT</Nav.Link>
+                    <Nav.Link href="#" className="mx-4 my-auto">LOGIN</Nav.Link>
+
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+            </Navbar>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/Components/HomePage.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+
+function HomePage(){
+    return (
+        <>
+            <Container className="carousels px-5 text-center py-auto mb-0" fluid="xs">
+            <h1 className="league-spartan-500 pt-5">WELCOME TO  <span className="luckiest-guy-regular">BACON REALMS</span></h1>
+            <h2 className="luckiest-guy-regular"><span>IP: </span>BACONREALMS.COM</h2>
+            </Container>
+
+            <Container className="home-about">
+
+                <div className="py-5 px-1 px-md-3 league-spartan-500">
+                    <h2>About Bacon Realms</h2>
+                    <h6 className="px-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere ipsam unde corrupti harum voluptate quaerat fuga nostrum est quae commodi veniam sequi laboriosam consectetur magnam laudantium, vitae consequuntur quis! Voluptatibus debitis eaque quod cupiditate neque iure laborum excepturi nesciunt, laboriosam labore, quasi illum, maxime eius eveniet accusamus amet ratione suscipit id hic dolorum? Laudantium tempore laboriosam quod, obcaecati exercitationem libero doloribus fugit quos quas magnam. Nam, accusamus blanditiis expedita voluptatibus, temporibus commodi repellat sapiente architecto incidunt assumenda saepe vero autem cupiditate sint unde, corporis et iste illum! Tempora, tenetur blanditiis!
+                        <br /> <br />
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ducimus aut magnam inventore veritatis odit nisi corrupti iste soluta! Officia, tenetur reprehenderit quis quibusdam illum praesentium exercitationem possimus, dolor incidunt accusamus, molestiae ad veritatis excepturi! Libero repudiandae qui maiores itaque nostrum. Autem animi corrupti laboriosam? Quasi commodi, cupiditate ipsam dicta dolorum facilis, labore debitis ducimus tenetur alias eos adipisci numquam?
+                    </h6>
+                </div>
+
+                <div className="py-5 px-1 px-md-3 league-spartan-500">
+                    <h2>How to Join?</h2>
+                    <h4 className="px-4">1. Add the Server</h4>
+                    <h6 className="px-5">Go to multiplayer in minecraft, then add server</h6>
+
+                    <h4 className="px-4">2. Enter Server Address</h4>
+                    <h6 className="px-5">Enter baconrealms.com in the server  address field and click done</h6>
+
+                    <h4 className="px-4">3. You're done!</h4>
+                    <h6 className="px-5">Now you can play on bacon realms with other</h6>
+                </div>
+            </Container>
+        
+        </>
+    )
+}
+
+export default HomePage;
